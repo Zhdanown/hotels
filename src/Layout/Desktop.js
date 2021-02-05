7a0c10aa36a35df029183c7bb2e7636e54b6bb16
ZhdanowN <zhdanown@gmail.com>
2021-02-05 13:25:05 +0300

    fix: change desktop layout (30/40/30)

--- src/Layout/Desktop.js
+++ src/Layout/Desktop.js
@@ -16,32 +16,19 @@ const HTMLOverflowHidden = createGlobalStyle`
 const DesktopLayoutContainer = styled.div`
   display: flex;
   overflow: hidden;
-  width: 120%;
 `;
 
 function LayoutDesktop({ children, currentStep, isShowStep }) {
   return (
     <DesktopLayoutContainer>
       <HTMLOverflowHidden />
-      <Column
-        active={currentStep === 1}
-        visible={isShowStep(1)}
-        currentStep={currentStep}
-      >
+      <Column active={currentStep === 1} visible={isShowStep(1)}>
         {children[0]}
       </Column>
-      <Column
-        active={currentStep === 2}
-        visible={isShowStep(2)}
-        currentStep={currentStep}
-      >
+      <Column active={currentStep === 2} visible={isShowStep(2)}>
         {children[1]}
       </Column>
-      <Column
-        active={currentStep === 3}
-        visible={isShowStep(3)}
-        currentStep={currentStep}
-      >
+      <Column active={currentStep === 3} visible={isShowStep(3)}>
         {children[2]}
       </Column>
     </DesktopLayoutContainer>
@@ -50,37 +37,44 @@ function LayoutDesktop({ children, currentStep, isShowStep }) {
 
 export default LayoutDesktop;
 
+const ColumnContainer = styled.div`
+  text-align: center;
+  padding: 1rem;
+  box-sizing: border-box;
+  margin: 0 auto;
+`;
+
 const StyledColumn = styled.div`
-  width: 40%;
+  width: 30%;
   height: calc(100vh - ${p => p.navbarHeight}px);
   transition: all 0.5s;
   overflow: hidden;
   opacity: ${p => (p.active ? 1 : 0.3)};
-  transform: translateX(-${p => (p.currentStep - 1) * 25}%);
+
+  ${ColumnContainer} {
+    max-width: 360px;
+  }
+
+  &:nth-child(2) {
+    width: 40%;
+    ${ColumnContainer} {
+      max-width: unset;
+    }
+  }
 `;
 
 const ColumnWrapper = styled.div`
   margin-right: -${p => p.scrollbarWidth}px;
   overflow-y: scroll;
   height: 100%;
-
-  .container {
-    text-align: center;
-    padding: 1rem;
-    box-sizing: border-box;
-  }
 `;
 
-function Column({ children, active, currentStep }) {
+function Column({ children, active }) {
   const navbarHeight = useNavbarHeight();
   return (
-    <StyledColumn
-      active={active}
-      currentStep={currentStep}
-      navbarHeight={navbarHeight}
-    >
+    <StyledColumn active={active} navbarHeight={navbarHeight}>
       <ColumnWrapper scrollbarWidth={scrollbarWidth} className="column-wrapper">
-        <div className="container">{children}</div>
+        <ColumnContainer>{children}</ColumnContainer>
       </ColumnWrapper>
     </StyledColumn>
   );
