7ef90c7af0357cf3854af08f36434e70d3918a78
ZhdanowN <zhdanown@gmail.com>
2021-02-08 17:46:46 +0300

    fix: add opacity to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,16 +2,14 @@ import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 
 import { CaretDownFilled } from "@ant-design/icons";
+import { useSelector } from "react-redux";
+import { getPrimaryColor } from "../redux/hotelConfig";
 
-const SPEED = "0.2s";
+const SPEED = 0.4;
 
 const AccordionSection = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-const TitleWrapper = styled.div`
-  transition: background-color ${SPEED} ease;
 `;
 
 export const Title = styled.p`
@@ -22,26 +20,32 @@ export const Title = styled.p`
   display: flex;
   align-items: center;
   transition: transform 0.2s;
+`;
 
-  &:hover {
-    transform: scale(1.1);
+const TitleWrapper = styled.div`
+  transition: background-color ${SPEED}s ease;
+
+  ${Title}:hover {
+    color: ${p => p.color};
   }
 `;
 
 export const Icon = styled(CaretDownFilled)`
   margin-left: 0.5rem;
-  transition: transform ${SPEED} ease;
+  transition: transform ${SPEED}s ease;
   ${p => (p.active ? "transform: rotate(180deg);" : "")}
 `;
 
 const ContentWrapper = styled.div`
   background-color: white;
   overflow: hidden;
-  transition: max-height ${SPEED} ease;
+  transition: max-height ${SPEED}s ease, opacity ${SPEED * 2}s;
   max-height: ${p => p.height};
+  opacity: ${p => (p.active ? 1 : 0)};
 `;
 
 function Accordion({ renderTitle, children, opened = false }) {
+  const color = useSelector(getPrimaryColor);
   const [active, setActive] = useState(opened);
   const [height, setHeight] = useState("0px");
 
@@ -57,10 +61,10 @@ function Accordion({ renderTitle, children, opened = false }) {
 
   return (
     <AccordionSection>
-      <TitleWrapper active={active ? 1 : 0}>
+      <TitleWrapper active={active ? 1 : 0} color={color}>
         {renderTitle && renderTitle(toggleAccordion, active)}
       </TitleWrapper>
-      <ContentWrapper ref={content} height={height}>
+      <ContentWrapper ref={content} height={height} active={active}>
         {children}
       </ContentWrapper>
     </AccordionSection>
