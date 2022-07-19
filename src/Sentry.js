2728d75aaff94dd3cd6a5655435833bb84747309
davidsoft <davidsoft46@gmail.com>
2022-07-19 09:10:40 +0300

    feat: update sentry key

--- src/Sentry.js
+++ src/Sentry.js
@@ -2,8 +2,8 @@ import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: "https://93a43fdf79184bd4bb45f648a7112f90@o507234.ingest.sentry.io/5710625",
+  dsn: "https://21a5a7f6d05f4e5d97c22c56f59100f3@o1316997.ingest.sentry.io/6569943",
   integrations: [new Integrations.BrowserTracing()],
-  
+
   tracesSampleRate: 1.0,
 });
