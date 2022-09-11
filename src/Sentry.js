9cad20b777ea4119e06d9318a5d4b4906044d447
zhdanown <zhdanown@gmail.com>
2022-09-11 13:36:55 +0300

    fix: init sentry in production environment

--- src/Sentry.js
+++ src/Sentry.js
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: "https://21a5a7f6d05f4e5d97c22c56f59100f3@o1316997.ingest.sentry.io/6569943",
-  integrations: [new Integrations.BrowserTracing()],
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://21a5a7f6d05f4e5d97c22c56f59100f3@o1316997.ingest.sentry.io/6569943",
+    integrations: [new Integrations.BrowserTracing()],
 
-  tracesSampleRate: 1.0,
-});
+    tracesSampleRate: 1.0,
+  });
+}
