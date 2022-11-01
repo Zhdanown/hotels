46c9c6dc31d4f4e000c08ebaa4aa6440eecf34a5
zhdanown <zhdanown@gmail.com>
2022-11-01 21:03:13 +0300

    fix: clear promo_block

--- src/Step1/Promocode.tsx
+++ src/Step1/Promocode.tsx
@@ -43,7 +43,7 @@ export default function Promocode() {
   const cancelPromocode = () => {
     toggle(false);
     dispatch(setPromocodeError(false));
-    dispatch(changeParams({ promo_code: "" }));
+    dispatch(changeParams({ promo_code: "", block: null }));
   };
 
   return (
@@ -89,7 +89,7 @@ const Form2 = () => {
     dispatch(setPromocodeError(!isValid));
 
     if (!isValid) {
-      dispatch(changeParams({ promo_code: "" }));
+      dispatch(changeParams({ promo_code: "", block: null }));
       return;
     }
 
